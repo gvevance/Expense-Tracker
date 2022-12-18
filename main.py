54d9aef249ac15c2d1b20266a2c31bb6d0324d05
gvevance <gvevance@gmail.com>
2022-12-18 11:38:19 +0530

    email login code added

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import subprocess
 subprocess.call("pwd")
 
-# email. detect subject. 
+# email. detect subject.
+
+import imaplib,email
+
+'''comment out later'''
+import sys
+applic_spec_password = sys.argv[1]
+'''comment out later'''
+
+imap_url = "imap.gmail.com"
+email = 'gvevance@gmail.com'
+
+def connect_to_server(imap_url,user,password):
+    ''' connecting to server '''
+    connection = imaplib.IMAP4_SSL(imap_url)
+    try :
+        connection.login(user,password)
+
+    except Exception as e:
+        print(e)
+        exit()
+    
+    return connection
+
+a = connect_to_server(imap_url,email,applic_spec_password)
+print(a)
