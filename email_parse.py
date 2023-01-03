2150f14f49eca733564ef7f1958b5c4a2d908958
gvevance <gvevance@gmail.com>
2023-01-03 07:53:10 +0530

    Sodexo payments checkpoint

--- email_parse.py
+++ email_parse.py
@@ -4,6 +4,7 @@ from email.parser import BytesParser
 from email.policy import default
 import re
 from datetime import datetime
+from datetime import timedelta
 from bs4 import BeautifulSoup
 
 parser = BytesParser(policy=default)
@@ -127,8 +128,97 @@ def parse_PhonePe_email_2(msg) :
     return
 
 
+def process_Sodexo_email_body(text,email_date,type) :
+    
+    if type == "text/plain" :
+
+        text_no_space = re.sub(' [ \t]+','\n',text)
+        #* logic = if two or more spaces come together, it is a long space and is replaced with a newline to split at in the next step 
+        
+        lines = text_no_space.splitlines()
+        # print(lines)
+
+        Date_txt = lines[2]
+        
+        txn_id_index = lines.index('Txn. ID')       # some names come in multiple lines
+        Amount_txt = lines[txn_id_index-1]
+        
+        Paid_to_txt = " ".join(lines[4:txn_id_index-1])
+
+        hi_message_index = lines.index('Hi Gabriel Ve Vance')
+        if lines[hi_message_index-1] != ':' :
+            Transaction_msg_txt = lines[hi_message_index-1]
+        else :
+            Transaction_msg_txt = ''
+        
+        try :
+            currency_char = Amount_txt[0]
+            amount_float = float(Amount_txt[2:])
+            datetime_object = datetime.strptime(Date_txt, '%b %d, %Y')
+        except :
+            currency_char = '$'
+            amount_float = 0.0
+            datetime_object = datetime.strptime('Jan 1, 2023', '%b %d, %Y')
+
+        print("\nDate : ",datetime_object)
+        print("Paid to : ",Paid_to_txt)
+        print("Amount : ",currency_char,amount_float)
+        print("Transaction message : ",Transaction_msg_txt)
+
+    elif type == "text/html" :
+        
+        soup = BeautifulSoup(text,'html.parser')
+        text_str = soup.text
+        # print(text_str)
+
+        text_no_space = re.sub('[\n]+','\n',text_str)
+        text_no_space = re.sub(' [ ]+','\n',text_no_space)
+        lines = text_no_space.splitlines()
+        # print(lines)
+
+        lines2 = []
+        for x in lines :
+            if x not in ['',' ','\xa0'] :
+                lines2.append(x.strip())
+        # print(lines2)
+
+        # Date_txt = ''       #! write code
+        
+        TotalAmount_index = lines2.index('Total Amount')
+        Amount_txt = lines2[TotalAmount_index+1]
+
+        Paid_to_txt = lines2[2].split('. Please call')[0].split(' at ')[-1]
+
+        # for i in range(len(lines2)) :
+        #     if lines2[i].startswith('Receipt ID: ') :
+        #         Date_txt = lines2[i+1]
+        #         break
+        
+        try :
+            currency_char = Amount_txt[0]
+            amount_float = float(Amount_txt[1:])
+            # datetime_object = datetime.strptime(Date_txt,'%H:%M,%d %b')
+        except :
+            currency_char = '$'
+            amount_float = 0.0
+            # datetime_object = datetime.strptime('Jan 1, 2023', '%b %d, %Y')
+
+        # print("\nDate1 : ",datetime_object)
+        print("\nDate : ",email_date)
+        print("Paid to : ",Paid_to_txt)
+        print("Amount : ",currency_char,amount_float)
+
+
 def parse_Sodexo_email(msg) :
-    # body = msg.get_payload(decode=True)
-    # body = str(body.decode())
-    # process_email_body_whitespaces(body)
-    return
+    email_date = datetime.strptime(msg['date'].split(' +')[0],'%a, %d %b %Y %H:%M:%S') + timedelta(minutes=30, hours=5)
+    for part in msg.walk():       
+        if part.get_content_type() == "text/plain":
+            body = part.get_payload(decode=True) #to control automatic email-style MIME decoding (e.g., Base64, uuencode, quoted-printable)
+            body = str(body.decode())
+            process_Sodexo_email_body(body,email_date,"text/plain")
+
+        elif part.get_content_type() == "text/html":
+            body = part.get_payload(decode=True)
+            body = str(body.decode())
+            process_Sodexo_email_body(body,email_date,"text/html")
+                
--- main.py
+++ main.py
@@ -11,27 +11,27 @@ from email_parse import parse_PhonePe_email_2
 from email_parse import parse_Sodexo_email
 from setup import update_seen_message_ids
 
-TESTING = True
 
 # return an imap connection object
-connection = connect_to_server()
-seen_messages_dict = get_seen_message_ids()
+try :
+    connection = connect_to_server()
+# except socket.gaierror :
+    # print("[Errno -3] Temporary failure in name resolution.")
+except :    
+    print("Some other error.")
 
-if TESTING :
-    seen_messages_dict = {}     #todo remove after testing
+seen_messages_dict = get_seen_message_ids()
 
 #select INBOX for PhonePe sent or paid
 status,messages = connection.select("\"PhonePe sent or paid\"")
 num_of_messages = int(messages[0])
 
-# if TESTING :
-#     num_of_messages = 2     #todo remove after testing
-
+num_of_messages = 0     #todo comment
 for i in range(num_of_messages,0,-1):
     res, msg, msg_ID = get_message_ID(connection,i)
     if msg_ID not in seen_messages_dict :
         seen_messages_dict[msg_ID] = True
-        parse_PhonePe_email_1(msg)
+        # parse_PhonePe_email_1(msg)
     else :
         # print("PhonePe payments processed.")
         break
@@ -40,9 +40,7 @@ for i in range(num_of_messages,0,-1):
 status,messages = connection.select("\"PhonePe Payment for\"")
 num_of_messages = int(messages[0])
 
-if TESTING :
-    num_of_messages = 1     #todo remove after testing
-
+num_of_messages = 0     #todo comment
 for i in range(num_of_messages,0,-1):
     res, msg, msg_ID = get_message_ID(connection,i)
     if msg_ID not in seen_messages_dict :
@@ -52,13 +50,14 @@ for i in range(num_of_messages,0,-1):
         print("PhonePe payments processed.")
         break
 
+
+seen_messages_dict = {}     #todo comment
 #select INBOX for Sodexo
 status,messages = connection.select("\"Sodexo payments\"")
 num_of_messages = int(messages[0])
 
-if TESTING :
-    num_of_messages = 1     #todo remove after testing
 
+# num_of_messages = 1     #todo comment
 for i in range(num_of_messages,0,-1):
     res, msg, msg_ID = get_message_ID(connection,i)
     if msg_ID not in seen_messages_dict :
@@ -68,4 +67,5 @@ for i in range(num_of_messages,0,-1):
         print("Sodexo payments processed.")
         break
 
-update_seen_message_ids(seen_messages_dict )
+#todo uncomment
+# update_seen_message_ids(seen_messages_dict)
